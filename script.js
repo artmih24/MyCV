bf1c22905ea1dc4fd16b6d16337b5287c1ea1947
artmih24 <83209415+artmih24@users.noreply.github.com>
2021-12-16 16:05:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,14 @@ function DownloadMyCV() {
 function ViewMyCV() {
     window.open('Резюме.pdf');
 }
+
+var mediaQuery = window.matchMedia("(max-device-width: 896px) and (-webkit-min-device-pixel-ratio: 2)");
+var bottomDiv = document.getElementById("bottom_div");
+
+if (mediaQuery.matches) {
+    console.log("mobile");
+    bottomDiv.innerHTML = '<button class="bottom_button" onclick="ViewMyCV()">Версия PDF</button>';
+} else {
+    console.log("desktop");
+    bottomDiv.innerHTML = '<button class="bottom_button" onclick="ViewMyCV()">Версия PDF</button> &nbsp;<button class="bottom_button" onclick="DownloadMyCV()">Загрузить</button>';
+}

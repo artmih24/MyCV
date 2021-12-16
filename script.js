556fd51cef735bd14814c14fd920558dcfdc5b10
artmih24 <83209415+artmih24@users.noreply.github.com>
2021-12-16 15:20:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const download = (path, filename) => {
+    // Create a new link
+    const anchor = document.createElement('a');
+    anchor.href = path;
+    anchor.download = filename;
+
+    // Append to the DOM
+    document.body.appendChild(anchor);
+
+    // Trigger `click` event
+    anchor.click();
+
+    // Remove element from DOM
+    document.body.removeChild(anchor);
+};
+
+function DownloadMyCV() {
+    download('Резюме.pdf', 'Резюме.pdf');
+}
+
+function ViewMyCV() {
+    window.open('Резюме.pdf');
+}
